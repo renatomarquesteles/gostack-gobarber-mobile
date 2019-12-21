92633e61b1dca1bf5dfe9fca06fb5a2be089c473
Renato <renatomarquesteles@gmail.com>
2019-12-21 09:19:22 -0300

    feat: adiciona background com linear gradient

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#5729d9', '#280868'],
+})`
+  flex: 1;
+`;
