1d79027d78a4228d262e2be27942c33d8c0cd795
Renato <renatomarquesteles@gmail.com>
2019-12-30 16:00:02 -0300

    configura code push

--- android/app/src/main/java/com/gobarber/MainApplication.java
+++ android/app/src/main/java/com/gobarber/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -29,8 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        protected String getJSMainModuleName() {
-          return "index";
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
         }
       };
 
